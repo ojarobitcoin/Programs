d23b0ae69f0e9fd9a2d73073265e487e63a4083a
Guimbreon <103893299+Guimbreon@users.noreply.github.com>
2022-11-26 00:06:45 +0000

    Add files via upload

---
+++ Rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+#Rock, paper and scissors
+import random
+import os
+#Global variables
+points=0
+points_bot=0
+i=0
+all_you=["0",]*3
+all_bot=["0",]*3
+#Os identification system
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    clear="clear"
+    wait=""
+elif platform == "darwin":
+    clear="clear"
+    wait=""
+elif platform == "win32":
+    clear="cls"
+    wait="pause"
+#Program start
+def whole():
+    global points_bot, points, all_you,all_bot #points out the global variables to use it
+    choices=["rock","paper","scissors"]
+    your_choice=input("What's your choice?\nrock\npaper\nscissors\n=")
+    all_you[i]=your_choice
+    if your_choice not in choices:
+        print("Your input was incorrect, try again!\n")
+        quit()
+    #Machine choice
+    machine_choice=random.choice(choices)
+    all_bot[i]=machine_choice
+    print(f"\n\n\nThe machine choice was {machine_choice} and yours was {your_choice}.")
+    #your_choice is paper
+    if your_choice=="paper" and machine_choice=="rock":
+        print("u won\n")
+        points+=1
+    elif your_choice=="paper" and machine_choice=="scissors":
+        print("u lost\n")
+        points_bot+=1
+    elif your_choice==machine_choice: #This goes for every choice in this game
+        print("it was a draw\n") #it was a draw which means no-one gets points
+    #your_choice is rock
+    if your_choice=="rock" and machine_choice=="paper":
+        print("u lost\n")
+        points_bot+=1
+    elif your_choice=="rock" and machine_choice=="scissors":
+        print("u won\n")
+        points+=1
+    #your_choice is scissors
+    if your_choice=="scissors" and machine_choice=="rock":
+        print("u lost\n")
+        points_bot+=1
+    elif your_choice=="scissors" and machine_choice=="paper":
+        print("u won\n")
+        points+=1
+def final():
+    print("The log of the game:\n")
+    print(f"U choosed {all_you[0]} and the bot choosed {all_bot[0]} on the first game.")
+    print(f"U choosed {all_you[1]} and the bot choosed {all_bot[1]} on the second game.")
+    print(f"U choosed {all_you[2]} and the bot choosed {all_bot[2]} on the third game.\n\n")
+while i < 3:
+    choices=["rock","paper","scissors"]
+    whole()
+    i+=1   
+    os.system(wait) #pauses the system until further notice
+    os.system(clear)  #clears all of the code above(just to make it look cleaner)
+    print(f"U have {points} and the bot has {points_bot}")
+os.system(wait)
+if points>points_bot:
+    print("\n\n\n\n\n")
+    final()
+    print("U won the best of 3 game of Rock, paper and scissors.\n")
+elif points<points_bot:
+    print("\n\n\n\n\n")
+    final()
+    print("U lost the best of 3 game of Rock, paper and scissors.\n")
+else:
+    print("\n\n\n\n\n")
+    final()
+    print('\033[1m'+"The best of 3 game of Rock, paper and scissors was a draw.\n"+'\033[1m' )
